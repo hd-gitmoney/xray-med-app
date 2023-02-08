18fb75faab378b9fbbb9e92f5c66c0eff6c416fa
Anas El Yousfi <anaselyousfi@gmail.com>
2023-02-07 20:02:48 -0500

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
+import Navbar from './components/NavBar'
 
 import { useApi } from './hooks/use-api';
 
 function App() {
   const { response } = useApi();
+  const data = 'patientData.json';
 
   return (
+    
     <div className="App">
+      
+    
+        
+
+      <Navbar/>
+
+
       <header className="App-header">
         <p>
           {response}
         </p>
+      
       </header>
     </div>
   );
