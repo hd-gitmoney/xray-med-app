b7646cad89e91589418a0e46300d5bc8c84cd37e
Pamela Caguana <pamelacaguana@gmail.com>
2023-02-26 23:15:35 -0500

    Merge anas-main to pam-main.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// const app = express()
+// app.listen(3001, () => {
+//   console.log("api runs")
+// });
+
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.send('API is working properly!');
