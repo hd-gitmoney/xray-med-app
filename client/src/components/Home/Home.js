95d09370412a8691b3b1c01a32906106c340857a
Manuela Sanchez <m.sanchez947@Hotmail.com>
2023-03-01 13:13:26 -0500

    home/admin routes to one single exam

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,7 @@
-// import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
-import Placeholder from 'react-bootstrap/Placeholder';
+// import Placeholder from 'react-bootstrap/Placeholder';
+
 //TODO
 //https://react-bootstrap.github.io/components/placeholder/
 
@@ -9,43 +9,41 @@ import Placeholder from 'react-bootstrap/Placeholder';
 
 const Home = ({exams}) => {
     return(
-        <Table striped="columns" bordered hover>
-            <thead>
-                <tr>
-                    <th>Patient ID</th>
-                    <th>Age</th>
-                    <th>Sex</th>
-                    <th>Zip</th>
-                    <th>Latest BMI</th>
-                    <th>Latest Weight</th>
-                    <th>Image</th>
-                    <th>Exam ID</th>
-                    <th>ICU Admit</th>
-                    <th># ICU Admits</th>
-                    <th>Mortality</th>
-                </tr>
-            </thead>
-            <tbody>
-            {exams.map((exam) => {
-                    return <tr key={exam._id}>
-                        <td><Link>{exam.PATIENT_ID}</Link></td>
-                        <td>{exam.AGE}</td>
-                        <td>{exam.SEX}</td>
-                        <td>{exam.ZIP}</td>
-                        <td>{exam.LATEST_BMI}</td>
-                        {/* having trouble showing these but I think its a naming convention problem */}
-                        <td>{exam.LATESTWEIGHT}</td>
-                        <td><img src={`https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/${exam.png_filename}`} className="xrayImages"/></td>
-                        <td>{exam.exam_Id}</td>
-                        {/* having trouble showing these but I think its a naming convention problem */}
-                        <td>{exam.ICU_Admit}</td> 
-                        <td>{exam.NUM_ICU_admits}</td>
-                        <td>{exam.MORTALITY}</td>
-                    </tr>
-                })}
-            </tbody>
-        </Table>
-        );
+                <Table striped="columns" bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Patient ID</th>
+                            <th>Age</th>
+                            <th>Sex</th>
+                            <th>Zip</th>
+                            <th>Latest BMI</th>
+                            <th>Latest Weight</th>
+                            <th>Image</th>
+                            <th>Exam ID</th>
+                            <th>ICU Admit</th>
+                            <th># ICU Admits</th>
+                            <th>Mortality</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {exams.map((exam) => {
+                            return <tr key={exam._id}>
+                                <td><Link to={`/examdetails/${exam._id}`}>{exam.PATIENT_ID}</Link></td>
+                                <td>{exam.AGE}</td>
+                                <td>{exam.SEX}</td>
+                                <td>{exam.ZIP}</td>
+                                <td>{exam.LATEST_BMI}</td>
+                                <td>{exam.LATESTWEIGHT}</td>
+                                <td><img src={`https://ohif-hack-diversity-covid.s3.amazonaws.com/covid-png/${exam.png_filename}`} className="xrayImages" alt="Chest X-Ray"/></td>
+                                <td>{exam.exam_Id}</td>
+                                <td>{exam.ICU_Admit}</td> 
+                                <td>{exam.NUM_ICU_admits}</td>
+                                <td>{exam.MORTALITY}</td>
+                            </tr>
+                        })}
+                    </tbody>
+                </Table>
+         );
     }
  
 export default Home;
