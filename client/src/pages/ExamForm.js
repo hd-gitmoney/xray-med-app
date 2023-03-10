bd7838de82368689740dfc3ab93bbf3e080e86ef
Pamela Caguana <pamelacaguana@gmail.com>
2023-03-09 21:21:25 -0500

    Add container to form

--- client/src/pages/ExamForm.js
+++ client/src/pages/ExamForm.js
@@ -1,10 +1,13 @@
 import { NewExamForm } from "../components/FormComponent/NewExamForm";
+import { Container } from "react-bootstrap";
 
 const ExamForm = () => {
     return(
         <div>
+            <Container>
             <h1>New Exam Form</h1>
                 <NewExamForm />
+            </Container>
         </div>
     )
 }
