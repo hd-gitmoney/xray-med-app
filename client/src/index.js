e746025ea635a8c8857e64a6de9ed86707728e5f
Pamela Caguana <pamelacaguana@gmail.com>
2023-03-09 20:02:42 -0500

    Change start script

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,9 @@ import { ExamsContextProvider } from './context/ExamsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <ExamsContextProvider>
         <App />
     </ExamsContextProvider>
+  </React.StrictMode>
 );
