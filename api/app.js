56c63d85c14af3072cb8002b3775386741f08999
Pamela Caguana <pamelacaguana@gmail.com>
2023-03-09 20:28:40 -0500

    Attempt to render paths

--- api/app.js
+++ api/app.js
@@ -19,15 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/', examsRouter);
-
-// PATH CONFIGURATION TO RESPOND TO A REQUEST TO STATIC ROUTE REQUEST BY SERVING index.html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 // Connect to Database
 mongoose.set("strictQuery", false);
